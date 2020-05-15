c851c8e6539a308081eb692860af2657895fd0d1
Van Le Huy <vanlehuy@Vans-Mac-mini.local>
2020-05-15 13:31:28 +0700

    finish version1

--- cakephp-4-0-3/templates/Feeds/test.php
+++ cakephp-4-0-3/templates/Feeds/test.php
@@ -1,10 +1,16 @@
 <html>
 <body>
 <form action ="test" method="post">
-<input type="hidden" name="token" value="EOwFwC4TIIydMVUHMXZZdkbUR0cluRSkFzecQy3m5pMTYVXRkcFIBWUZYLNUNSNgQKdnpTWu" />
+<!-- <input type="hidden" name="token" value="EOwFwC4TIIydMVUHMXZZdkbUR0cluRSkFzecQy3m5pMTYVXRkcFIBWUZYLNUNSNgQKdnpTWu" /> -->
 Name: <input type="text" name="name"><br>
 Message: <input type="text" name="message"><br>
 <input type="submit">
+<link
+    href="https://shannon1102.github.io/Sinhnhatbeyeu/"
+    title="Comments"
+    type="application/rss+xml"
+    rel="alternate"
+/>
 </form>
 </body>
 </html>
